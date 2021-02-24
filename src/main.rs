a6d5e6bc1ea3cba6079a4e8a5123ba0b187b5826
Gautham Velchuru <gvelchuru@gmail.com>
2021-02-24 15:17:59 -0800

    switch to main wikipedia api, add capability for writing in an artist

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
 use std::io;
 extern crate reqwest;
 extern crate json;
-extern crate scraper;
+extern crate regex;
+extern crate itertools;
 use reqwest::Client;
-use std::collections::HashMap;
-
-use scraper::{Html, Selector};
+use regex::Regex;
+use itertools::join;
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
+
+    let re = match Regex::new(r"birth_place\s*=\s*([^\\n]*)\\n")
+    {
+        Ok(re) => re,
+        Err(err) => {
+            println!("{}", err);
+            panic!(err)
+        }
+    };
+
+    let script_regex = match Regex::new(r"<[^>]*>")
+    {
+        Ok(re) => re,
+        Err(err) => {
+            println!("{}", err);
+            panic!(err)
+        }
+    };
+
     let mut num_artists = String::new();
     println!("How many artists?");
 
@@ -18,6 +37,8 @@ async fn main() -> Result<(), reqwest::Error> {
 
     let mut num_artists: u32 = num_artists.trim().parse().expect("Please type a number!");
     let mut artists = Vec::new();
+    
+    
     
     while num_artists > 0 {
         println!("Next artist");
@@ -36,7 +57,7 @@ async fn main() -> Result<(), reqwest::Error> {
 
 
     for artist in artists {
-        let location = loc(&artist, &client);
+        let location = loc(&artist, &client, &re, &script_regex);
         //match location {
             //Some(x) => println!("{}", x),
             //None => {},
@@ -46,15 +67,18 @@ async fn main() -> Result<(), reqwest::Error> {
     Ok(())
 }
 
-async fn loc(name: &str, client: &Client) -> Result<String, reqwest::Error>
+async fn loc(name: &str, client: &Client, re: &Regex, script_regex: &Regex) -> Result<String, reqwest::Error>
 {
     //let mut wiki_name = name.split(" ").collect();
-    let wiki_name = "Metro_Boomin";
-    let wiki_url = &(format!("https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles={}&rvsection=0", wiki_name)).to_string();
+    let wiki_name = join(name.split(" "), "_");
+    let wiki_url = &(format!("https://en.wikipedia.org/w/api.php?action=parse&page={}&format=json", wiki_name)).to_string();
     let resp = client.get(wiki_url).send().await?.text().await?;
-    let pages = &json::parse(&resp).unwrap()["query"]["pages"];
-    let entry = &pages.entries().next().unwrap().1["revisions"][0]["*"];
-    println!("{}", entry.pretty(2));
+    let entry = &json::parse(&resp).unwrap()["parse"]["text"]["*"].dump();
+    let replaced = script_regex.replace_all(entry, "");
+    //let entry = &pages.entries().next().unwrap().1["revisions"][0]["*"].dump();
+    println!("{:?}", replaced);
+    //let loc = &re.captures(entry).unwrap()[1].replace("[","").replace("]","");
+    //println!("{:?}", loc);
     
     Ok("".to_string())
 }
